22c1c6da46615d4358e5f3fc781a92435f190380
zhangzetao <15262040158@163.com>
2020-05-03 00:09:20 +0800

    leetCode 2020-05-02 :pencil2: 10 coins

--- src/com/company/simple/LeetCode_3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/company/simple/LeetCode_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,73 +1,26 @@
 package com.company.simple;
 
-import java.util.Arrays;
-
-/**
- * 无重复字符的最长子串
- */
+import java.util.HashMap;
+import java.util.Map;
 
 class LeetCode_3_Solution {
-
-    public int lengthOfLongestSubstring0MS(String s) {
+    public int lengthOfLongestSubstring(String s) {
         if (s == null)
             return 0;
-
-        byte[] arrays = new byte[128];
-        int n = s.length(), ans = 0, i = 0, j = 0, c;
-        while (j < n) {
-            c = s.charAt(j);
-            if (i < arrays[c]) {
-                if (ans < (byte)j - i)
-                    ans = (byte)j - i;
-                i = arrays[c];
+        int ans = 0;
+        Map<Character, Integer> hashMap = new HashMap<>();
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            if (hashMap.containsKey(c)) {
+                start = Math.max(start, hashMap.get(c));
             }
-            arrays[c] = (byte) ++j;
+            ans = Math.max(ans, end - start + 1);
+            hashMap.put(c, end + 1);
         }
-        if (ans < (byte)j - i)
-            ans = (byte)j - i;
         return ans;
     }
-
-    public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) return 0;
-        if (s.length() == 1) return 1;
-        int[] subStrArr = new int[127 - 32 + 1];
-        int len = 0;
-        int max = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int k = i;
-            while (k < s.length()) {
-                int index = s.charAt(k) - ' ';
-                subStrArr[index] ++;
-                if (subStrArr[index] > 1) {
-                    Arrays.fill(subStrArr, 0);
-                    break;
-                }
-                len++;
-                k++;
-            }
-            if (len > max) max = len;
-            len = 0;
-        }
-
-        return max;
-
-    }
-
 }
 
 public class LeetCode_3_LongestSubstringWithoutRepeatingCharacters {
-
-    public static void main(String[] args) {
-        LeetCode_3_Solution solution = new LeetCode_3_Solution();
-        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
-        System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
-        System.out.println(solution.lengthOfLongestSubstring(" "));
-        System.out.println(solution.lengthOfLongestSubstring(""));
-        System.out.println(solution.lengthOfLongestSubstring(null));
-        System.out.println(solution.lengthOfLongestSubstring("aab"));
-        System.out.println(solution.lengthOfLongestSubstring("dvdf"));
-    }
 
 }
