bb9eb78f9158c0980a50066bde7b98608a99d715
zhangzetao <zhangzetao@wscn.work>
2021-04-21 20:27:57 +0800

    add

---
+++ 2021/04/numDecodings.go
@@ -0,0 +1,16 @@
+package main
+
+func numDecodings(s string) int {
+	n := len(s)
+	f := make([]int, n+1)
+	f[0] = 1
+	for i := 1; i <= n; i++ {
+		if s[i-1] != '0' {
+			f[i] += f[i-1]
+		}
+		if i > 1 && s[i-2] != '0' && ((s[i-2]-'0')*10+(s[i-1]-'0') <= 26) {
+			f[i] += f[i-2]
+		}
+	}
+	return f[n]
+}
