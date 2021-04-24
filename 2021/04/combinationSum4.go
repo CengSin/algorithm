9ce7d1161e5c38110803fa9de00ab251a72ec451
zhangzetao <zhangzetao@wscn.work>
2021-04-24 19:51:17 +0800

    leetcode_04_24

---
+++ 2021/04/combinationSum4.go
@@ -0,0 +1,20 @@
+package main
+
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+
+	for i := 1; i < target+1; i++ {
+		for _, num := range nums {
+			if i >= num {
+				dp[i] += dp[i-num]
+			}
+		}
+	}
+
+	return dp[target]
+}
+
+func main() {
+
+}
