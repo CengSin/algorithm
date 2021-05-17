af01c57d7a134bda332942f204d8dd661b1cba54
zhangzetao <zhangzetao@wscn.work>
2021-05-17 08:02:43 +0800

    判断两个节点是不是堂兄弟节点

---
+++ 2021/05/leetcode/editor/cn/993_二叉树的堂兄弟节点.go
@@ -0,0 +1,94 @@
+package main
+
+//在二叉树中，根节点位于深度 0 处，每个深度为 k 的节点的子节点位于深度 k+1 处。
+//
+// 如果二叉树的两个节点深度相同，但 父节点不同 ，则它们是一对堂兄弟节点。
+//
+// 我们给出了具有唯一值的二叉树的根节点 root ，以及树中两个不同节点的值 x 和 y 。
+//
+// 只有与值 x 和 y 对应的节点是堂兄弟节点时，才返回 true 。否则，返回 false。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//输入：root = [1,2,3,4], x = 4, y = 3
+//输出：false
+//
+//
+// 示例 2：
+//
+//
+//
+//输入：root = [1,2,3,null,4,null,5], x = 5, y = 4
+//输出：true
+//
+//
+// 示例 3：
+//
+//
+//
+//
+//输入：root = [1,2,3,null,4], x = 2, y = 3
+//输出：false
+//
+//
+//
+// 提示：
+//
+//
+// 二叉树的节点数介于 2 到 100 之间。
+// 每个节点的值都是唯一的、范围为 1 到 100 的整数。
+//
+//
+//
+// Related Topics 树 广度优先搜索
+// 👍 134 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isCousins(root *TreeNode, x int, y int) bool {
+	var xParent, yParent *TreeNode
+	var xDepth, yDepth int
+	var xFound, yFound bool
+
+	var dfs func(node, parend *TreeNode, depth int)
+	dfs = func(node, parend *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+
+		if node.Val == x {
+			xParent, xDepth, xFound = parend, depth, true
+		}
+		if node.Val == y {
+			yParent, yDepth, yFound = parend, depth, true
+		}
+
+		if xFound && yFound {
+			return
+		}
+
+		dfs(node.Left, node, depth+1)
+
+		if xFound && yFound {
+			return
+		}
+
+		dfs(node.Right, node, depth+1)
+	}
+
+	dfs(root, nil, 0)
+	return xDepth == yDepth && xParent != yParent
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
