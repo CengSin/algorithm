47e3e9c88a286fff3c64d8aa100f99098a1bfcc7
zhangzetao <zhangzetao@wscn.work>
2021-06-05 20:46:32 +0800

    移除链表中相同的元素

---
+++ 2021/05/leetcode/editor/cn/203_移除链表元素.go
@@ -0,0 +1,68 @@
+package main
+
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [], val = 1
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 列表中的节点在范围 [0, 104] 内
+// 1 <= Node.val <= 50
+// 0 <= k <= 50
+//
+// Related Topics 链表
+// 👍 632 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	var newRoot, root *ListNode
+	for head != nil {
+		if head.Val != val {
+			if newRoot == nil {
+				newRoot = &ListNode{
+					Val: head.Val,
+				}
+				root = newRoot
+			} else {
+				newRoot.Next = &ListNode{
+					Val: head.Val,
+				}
+				newRoot = newRoot.Next
+			}
+		}
+		head = head.Next
+	}
+	return root
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
