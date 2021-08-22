659c10ba209280f1ca0e02b4f55eabc114c1bd9b
zhangzetao <zhangzetao@wscn.work>
2021-08-22 21:03:22 +0800

    :construction: add

---
+++ 2021/08/leetcode/editor/cn/789逃脱阻碍者.go
@@ -0,0 +1,89 @@
+//你在进行一个简化版的吃豆人游戏。你从 [0, 0] 点开始出发，你的目的地是 target = [xtarget, ytarget] 。地图上有一些阻碍者，
+//以数组 ghosts 给出，第 i 个阻碍者从 ghosts[i] = [xi, yi] 出发。所有输入均为 整数坐标 。
+//
+// 每一回合，你和阻碍者们可以同时向东，西，南，北四个方向移动，每次可以移动到距离原位置 1 个单位 的新位置。当然，也可以选择 不动 。所有动作 同时 发生
+//。
+//
+// 如果你可以在任何阻碍者抓住你 之前 到达目的地（阻碍者可以采取任意行动方式），则被视为逃脱成功。如果你和阻碍者同时到达了一个位置（包括目的地）都不算是逃脱
+//成功。
+//
+// 只有在你有可能成功逃脱时，输出 true ；否则，输出 false 。
+//
+//
+// 示例 1：
+//
+//
+//输入：ghosts = [[1,0],[0,3]], target = [0,1]
+//输出：true
+//解释：你可以直接一步到达目的地 (0,1) ，在 (1, 0) 或者 (0, 3) 位置的阻碍者都不可能抓住你。
+//
+//
+// 示例 2：
+//
+//
+//输入：ghosts = [[1,0]], target = [2,0]
+//输出：false
+//解释：你需要走到位于 (2, 0) 的目的地，但是在 (1, 0) 的阻碍者位于你和目的地之间。
+//
+//
+// 示例 3：
+//
+//
+//输入：ghosts = [[2,0]], target = [1,0]
+//输出：false
+//解释：阻碍者可以和你同时达到目的地。
+//
+//
+// 示例 4：
+//
+//
+//输入：ghosts = [[5,0],[-10,-2],[0,-5],[-2,-2],[-7,1]], target = [7,7]
+//输出：false
+//
+//
+// 示例 5：
+//
+//
+//输入：ghosts = [[-1,0],[0,1],[-1,0],[0,1],[-1,0]], target = [0,0]
+//输出：true
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= ghosts.length <= 100
+// ghosts[i].length == 2
+// -10⁴ <= xi, yi <= 10⁴
+// 同一位置可能有 多个阻碍者 。
+// target.length == 2
+// -10⁴ <= xtarget, ytarget <= 10⁴
+//
+// Related Topics 数组 数学 👍 72 👎 0
+
+package main
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func escapeGhosts(ghosts [][]int, target []int) bool {
+	distance := manhattanDistance(0, 0, target[0], target[1])
+	for _, ghost := range ghosts {
+		// 如果有一个阻碍者的最短距离（曼哈顿距离） <= 玩家到终点的距离，则玩家会被抓住
+		if manhattanDistance(ghost[0], ghost[1], target[0], target[1]) <= distance {
+			return false
+		}
+	}
+	return true
+}
+
+func manhattanDistance(x0, y0, x1, y1 int) int {
+	abs := func(a int) int {
+		if a < 0 {
+			return -a
+		}
+		return a
+	}
+	return abs(x0-x1) + abs(y0-y1)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
