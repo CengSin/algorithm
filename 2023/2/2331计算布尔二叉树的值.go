8657470b392c87ba294b050cb451971ce8283cd0
zhangzetao <zhangzetao@wscn.work>
2023-02-06 18:48:11 +0800

    add

---
+++ 2023/2/2331计算布尔二叉树的值.go
@@ -0,0 +1,16 @@
+package main
+
+func evaluateTree(root *TreeNode) bool {
+	switch root.Val {
+	case 0:
+		return false
+	case 1:
+		return true
+	case 2:
+		return evaluateTree(root.Left) || evaluateTree(root.Right)
+	case 3:
+		return evaluateTree(root.Left) && evaluateTree(root.Right)
+	default:
+		return false
+	}
+}
